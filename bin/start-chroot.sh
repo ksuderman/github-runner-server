33f687fd4d0fcaf11649ed6387a8596227653fb8
Keith Suderman <suderman@jhu.edu>
2025-03-15 18:38:53 -0400

    Full path

--- bin/start-chroot.sh
+++ bin/start-chroot.sh
@@ -4,8 +4,9 @@ set -eu
 id=$1
 root=/var/chroot
 dir=$root/$id
+echo "Starting chroot $dir"
 mkdir -p $dir
-for d in $(cat MOUNTS) ; do
+for d in $(cat $root/MOUNTS) ; do
   mkdir $dir/$d
   mount --bind /$d $dir/$d
 done
