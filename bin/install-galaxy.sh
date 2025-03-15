0dbfe0d540c267678fcf4111ee591d4220c02e4d
Keith Suderman <suderman@jhu.edu>
2025-03-15 18:09:09 -0400

    Script to launch an AnVIL instance from a chroot jail

---
+++ bin/install-galaxy.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+ID=$1
+prefix="ks-github-$(cat /dev/urandom | tr -dc 'a-zA-Z0-9_+-' | head -c 8)"
+anvil cluster disks galaxy --prefix $prefix
+mkdir /tmp/$ID
+cp /root/.kube/config /tmp/$ID/kubeconfig
+kubectl get svc -n galaxy galaxy -o jsonpath='{.status.loadBalancer.ingress[0].ip}:{.spec.ports[0].port}' > /tmp/$ID/ip
+echo $prefix > /tmp/$ID/prefix
--- bin/start-chroot.sh
+++ bin/start-chroot.sh
@@ -1,5 +1,17 @@
 #!/usr/bin/env bash
 set -eu
 
-skeleton=/var/chroot/skeleton
-dir=/var/chroot/$(uuidgen)
+id=$(uuidgen)
+root=/var/chroot
+dir=$root/$id
+mkdir -p $dir
+for d in $(cat MOUNTS) ; do
+  mkdir $dir/$d
+  mount --bind /$d $dir/$d
+done
+cp /var/chroot/install-galaxy.sh $dir/
+chroot $dir install-galaxy.sh
+#for d in $(cat MOUNTS) ; do
+#  umount $dir/$d
+#done
+#rm -rf $dir
