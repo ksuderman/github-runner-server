077f16a9f3217d48c21e0bb09cc864c057d3eb70
Keith Suderman <suderman@jhu.edu>
2025-03-16 18:56:02 -0400

    Update status during install

--- bin/install-galaxy.sh
+++ bin/install-galaxy.sh
@@ -3,9 +3,16 @@ set -eu
 ID=$1
 DIR=/run/chroot/$ID
 mkdir $DIR
+status_file=$DIR/status
 prefix="ks-github-$(cat /dev/urandom | tr -dc 'a-z0-9' | head -c 8)"
 echo $prefix > $DIR/prefix
-anvil cluster disks galaxy --prefix $prefix
+echo "Creating cluster" > $status_file
+anvil cluster  --prefix $prefix
+echo "Creating disks" > $status_file
+anvil disks --prefix $prefix
+echo "Installing Galaxy" > $status_file
+anvil galaxy --prefix $prefix
 cp /root/.kube/config $DIR/kubeconfig
 service=$(kubectl get svc -n galaxy | grep nginx | awk '{print $1}')
 kubectl get svc -n galaxy $service -o jsonpath='{.status.loadBalancer.ingress[0].ip}:{.spec.ports[0].port}' > $DIR/ip
+echo "Ready" > $status_file
