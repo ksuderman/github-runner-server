40f007effb0b7a2860ba9c62d5fbeb268ef8cd1a
Keith Suderman <suderman@jhu.edu>
2025-03-17 15:04:39 -0400

    Don't ignore SIGPIPE

--- bin/watcher.sh
+++ bin/watcher.sh
@@ -9,14 +9,14 @@ while true; do
     if [[ -e $CREATE_DIR/$job ]]; then
       echo "Launching cluster for job $job"
       rm -f $CREATE_DIR/$job
-      nohup /home/ubuntu/github-webhook-server/bin/start-chroot.sh $job &
+      /home/ubuntu/github-webhook-server/bin/start-chroot.sh $job &
     fi
   done
   for job in $(ls $SHUTDOWN_DIR); do
     if [[ -e $SHUTDOWN_DIR/$job ]]; then
       echo "Stopping cluster for job $job"
       rm -f $SHUTDOWN_DIR/$job
-      nohup /home/ubuntu/github-webhook-server/bin/cleanup.sh $job &
+      /home/ubuntu/github-webhook-server/bin/cleanup.sh $job &
     fi
   done
   sleep 30
