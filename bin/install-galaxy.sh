81222e2cad4def2cd30606576a2676c4c149e134
Keith Suderman <suderman@jhu.edu>
2025-03-16 23:30:49 -0400

    Suppress newlines

--- bin/install-galaxy.sh
+++ bin/install-galaxy.sh
@@ -5,14 +5,14 @@ DIR=/run/chroot/$ID
 mkdir $DIR
 status_file=$DIR/status
 prefix="ks-github-$(cat /dev/urandom | tr -dc 'a-z0-9' | head -c 8)"
-echo $prefix > $DIR/prefix
-echo "Creating cluster" > $status_file
+echo -n $prefix > $DIR/prefix
+echo -n "Creating cluster" > $status_file
 anvil cluster  --prefix $prefix
-echo "Creating disks" > $status_file
+echo -m "Creating disks" > $status_file
 anvil disks --prefix $prefix
-echo "Installing Galaxy" > $status_file
+echo -n "Installing Galaxy" > $status_file
 anvil galaxy --prefix $prefix
 cp /root/.kube/config $DIR/kubeconfig
 service=$(kubectl get svc -n galaxy | grep nginx | awk '{print $1}')
 kubectl get svc -n galaxy $service -o jsonpath='{.status.loadBalancer.ingress[0].ip}:{.spec.ports[0].port}' > $DIR/ip
-echo "Ready" > $status_file
+echo -n "Ready" > $status_file
