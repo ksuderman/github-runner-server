07aa1e77fa829f9ffc33039f10e19bfa77e623a2
Keith Suderman <suderman@jhu.edu>
2025-03-17 23:08:37 -0400

    Updated scripts

--- bin/cleanup.sh
+++ bin/cleanup.sh
@@ -6,19 +6,19 @@ if [[ ! -e /var/chroot/$1 ]]; then
   echo "No such chroot: $1"
   exit 1
 fi
-if [[ -e /run/$1/prefix ]]; then
-  prefix=$(cat /run/$1/prefix)
+if [[ -e /run/chroot/$1/prefix ]]; then
+  prefix=$(cat /run/chroot/$1/prefix)
   echo "Deleting cluster $prefix"
   anvil cleanup delete-disks --prefix $prefix
 else
   echo "Unable to delete cluster. No prefix found for $1"
 fi
-for d in $(cat /var/chroot/MOUNTS) ; do
+for d in $(cat /home/ubuntu/github-webhook-server/bin//MOUNTS) ; do
   sudo umount /var/chroot/$1/$d
 done
-sudo rm -rf /var/chroot/$1
+sudo rm -r /var/chroot/$1
 
-if [[ -e /run/$1 ]]; then
-  sudo rm -rf /run/$1
+if [[ -e /run/chroot/$1 ]]; then
+  sudo rm -rf /run/chroot/$1
 fi
 
--- bin/setup.sh
+++ bin/setup.sh
@@ -12,10 +12,25 @@ for dir in /var/chroot /run/jobs/create /run/jobs/shutdown /run/chroot /etc/temp
   fi
 done
 chown -R ubuntu:ubuntu /run/jobs
+apt update
+curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | gpg --dearmor -o /usr/share/keyrings/cloud.google.gpg
+echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] https://packages.cloud.google.com/apt cloud-sdk main" | tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
+apt update
+apt install -y google-cloud-cli google-cloud-cli-gke-gcloud-auth-plugin
+
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+
+curl https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 | bash
+helm repo add anvil https://github.com/cloudve/helm-charts/raw/anvil
+
+git clone --branch chroot https://github.com/ksuderman/github-runner-server.git /home/ubuntu/github-webhook-server
+chown -R ubuntu:ubuntu /home/ubuntu/github-webhook-server
+
 cp bin/anvil /usr/local/bin
 cp bin/timer.sh /usr/local/bin
 cp templates/anvil-values.yml.j2 /etc/templates
 cp gunicorn.service /etc/systemd/system
 cp watcher.service /etc/systemd/system
 systemctl daemon-reload
-systemctl enable gunicorn
+#systemctl enable gunicorn
