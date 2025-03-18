80df6ba610701a4d7be86ea96c155645cb5be6c2
Keith Suderman <suderman@jhu.edu>
2025-03-18 00:08:14 -0400

    Fix cleanup

--- bin/cleanup.sh
+++ bin/cleanup.sh
@@ -2,10 +2,6 @@
 
 set -eu
 
-if [[ ! -e /var/chroot/$1 ]]; then
-  echo "No such chroot: $1"
-  exit 1
-fi
 if [[ -e /run/chroot/$1/prefix ]]; then
   prefix=$(cat /run/chroot/$1/prefix)
   echo "Deleting cluster $prefix"
@@ -13,11 +9,6 @@ if [[ -e /run/chroot/$1/prefix ]]; then
 else
   echo "Unable to delete cluster. No prefix found for $1"
 fi
-for d in $(cat /home/ubuntu/github-webhook-server/bin//MOUNTS) ; do
-  sudo umount /var/chroot/$1/$d
-done
-sudo rm -r /var/chroot/$1
-
 if [[ -e /run/chroot/$1 ]]; then
   sudo rm -rf /run/chroot/$1
 fi
